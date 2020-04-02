c642dd0bd6c2ed98b9e80586bc26c9e5bc898bd4
ahmetserdargeze <gezeahmetserdar@gmail.com>
2020-04-02 15:44:48 +0300

    master değişiklikleri yapıldı

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Master Branch'e değişiklik Yapıyoruz");
+        System.out.println("Master Branch'e ekstra değişiklik Yapıyoruz");
+        System.out.println("Master Branch'e dahada ekstra değişiklik Yapıyoruz");
     }
 }
